33d9faf647a829b5e9d4dbe7d463ffd7f8d06eac
Rootless <clementdud57@gmail.com>
2025-09-21 17:03:23 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# hello.py
+
+# This is a comment. The following code prints a message to the screen.
+
+def main():
+    print("Hello, World!")  # Prints "Hello, World!" to the console
+
+if __name__ == "__main__":
+    main()  # Calls the main() function if the file is run directly
